66067543eea67a640d72578141bc8e716c9a5dc5
MIJAIL <mijail.carlos.1991@gmail.com>
2022-05-05 23:34:54 -0500

    eje1

---
+++ Taller1/Ejercicio01.java
@@ -0,0 +1,17 @@
+package Taller1;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+        public static void main(String[] args) {
+        double altura;
+        double largo;
+        double ancho;
+        double costometrocubico;
+        double volumen;
+        double pago;
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("carlosssss22222222");
+    }
+}
